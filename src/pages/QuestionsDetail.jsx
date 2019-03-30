a757b417b8d0e7abbe94002fe52648472535ec88
Joseph Groves <grovesjosephn@gmail.com>
2019-03-30 15:15:34 -0600

    refactor questions detail to useHooks

--- src/pages/QuestionsDetail.jsx
+++ src/pages/QuestionsDetail.jsx
@@ -1,71 +1,66 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { Box, Button, Flex } from 'rebass'
 
 import { getQuestionById, postChoice } from "../api";
 import { H1, H2 } from '../components/Headings';
 import { Table, TD } from '../components/Table'
 
-class QuestionsPage extends Component {
+const QuestionsPage = (props) => {
+    const [question, setQuestion] = useState({})
+    const [allVotes, setAllVotes] = useState(0)
+    const [currentVote, setCurrentVote] = useState(null)
 
-    state = {
-        question: {},
-        allVotes: 0,
-        currentVote: null
+    useEffect(() => {
+        const fetchQuestion = async () => {
+            const { data } = await getQuestionById(props.match.params.id)
+            const allVotes = data.choices && data.choices.reduce((acm, { votes }) => acm + votes, 0)
+            setQuestion(data)
+            setAllVotes(allVotes)
+        }
+
+        fetchQuestion()
+    }, [])
+
+    const handleChoiceSelection = (i) => {
+        setCurrentVote(i)
     }
 
-    async componentDidMount() {
-        const result = await getQuestionById(this.props.match.params.id)
-        const allVotes = result.data.choices && result.data.choices.reduce((acm, { votes }) => acm + votes, 0)
-        this.setState({ question: result.data, allVotes })
-    }
-
-    handleChoiceSelection = (i) => {
-        this.setState({ currentVote: i })
-    }
-
-    handleVote = async () => {
-        const { question, currentVote } = this.state
+    const handleVote = async () => {
         if (currentVote != null) {
             await postChoice(question.choices[currentVote].url)
-            this.props.history.push('/')
+            props.history.push('/')
         }
     }
 
-
-    render() {
-
-        const { question, allVotes, currentVote } = this.state
-        return (
-            <Box mx={4}>
-                <H1 mb={2}>Question Details</H1>
-                <H2 mb={4}>Question: {question.question && question.question}</H2>
-                <Table style={{ width: "100%" }}>
-                    <thead>
-                        <tr>
-                            <TD as="th" fontWeight="bold">Choice</TD>
-                            <TD as="th" fontWeight="bold" textAlign="right">Votes</TD>
-                            <TD as="th" fontWeight="bold" textAlign="right">Percentage</TD>
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {
-                            question.choices && question.choices.map(({ choice, url, votes }, i) => (
-                                <tr key={i} onClick={() => this.handleChoiceSelection(i)} style={{ backgroundColor: i === currentVote ? "lightgrey" : "white" }}>
-                                    <TD>{choice}</TD>
-                                    <TD textAlign="right"><span>{votes}</span></TD>
-                                    <TD textAlign="right">{Math.round(votes / allVotes * 100)}%</TD>
-                                </tr>
-                            ))
-                        }
-                    </tbody>
-                </Table>
-                <Flex justifyContent="end" mt={4}>
-                    <Button bg={currentVote != null ? "grey" : "lightgrey"} borderRadius={1} onClick={() => currentVote != null && this.handleVote()}>Save vote</Button>
-                </Flex>
-            </Box >
-        )
-    }
-
+    return (
+        <Box mx={4}>
+            <H1 mb={2}>Question Details</H1>
+            <H2 mb={4}>Question: {question.question && question.question}</H2>
+            <Table style={{ width: "100%" }}>
+                <thead>
+                    <tr>
+                        <TD as="th" fontWeight="bold">Choice</TD>
+                        <TD as="th" fontWeight="bold" textAlign="right">Votes</TD>
+                        <TD as="th" fontWeight="bold" textAlign="right">Percentage</TD>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        question.choices && question.choices.map(({ choice, votes }, i) => (
+                            <tr key={i} onClick={() => handleChoiceSelection(i)} style={{ backgroundColor: i === currentVote ? "lightgrey" : "white" }}>
+                                <TD>{choice}</TD>
+                                <TD textAlign="right"><span>{votes}</span></TD>
+                                <TD textAlign="right">{Math.round(votes / allVotes * 100)}%</TD>
+                            </tr>
+                        ))
+                    }
+                </tbody>
+            </Table>
+            <Flex justifyContent="end" mt={4}>
+                <Button bg={currentVote != null ? "grey" : "lightgrey"} borderRadius={1} onClick={() => handleVote()}>Save vote</Button>
+            </Flex>
+        </Box >
+    )
 }
 
 export default QuestionsPage;
