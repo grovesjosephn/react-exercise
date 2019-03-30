7e9458e2f8731e593e6207777aee23fb9db09ad9
Joseph Groves <grovesjosephn@gmail.com>
2019-03-30 11:52:34 -0600

    Adds styled-components and rebass and updates questions

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -1,13 +1,25 @@
 import React from 'react'
+import { Card, Text } from 'rebass'
 
 function Question({ question, published_at, choices }) {
     return (
-        <div>
-            <div>
-                <span className="question__header">{question}</span><span className="question__choice">({choices.length})</span>
+        <Card
+            width={[1, 1 / 2, 1 / 3]}
+            p={4}
+            borderRadius={[0, 4]}
+            boxShadow={['', '0 2px 16px rgba(0, 0, 0, 0.25)']}
+        >
+            <div style={{ display: 'flex', flexDirection: 'row', justifyContent: "space-between" }} >
+                <Text
+                    fontSize={[2]}
+                    fontWeight='bold'
+                    style={{ textTransform: "uppercase" }}
+                >
+                    {question}
+                </Text> <Text fontSize={[1]} color="grey" fontWeight="bold">({choices.length})</Text>
             </div>
-            <span className="questions__published">{published_at}</span>
-        </div>
+            <Text fontSize={[1]} color="grey">{published_at}</Text>
+        </Card >
     )
 }
 
